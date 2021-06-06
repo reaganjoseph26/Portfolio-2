4a524ca689b4e170346f04fff933cd094b3d0411
Racer <reaganjoseph26@yahoo.com>
2021-06-06 18:33:06 -0400

    carousel right and and left arrows functioning

--- js/script.js
+++ js/script.js
@@ -12,20 +12,40 @@ const slidePosition = (slide, index) => {
 };
 slides.forEach(slidePosition);
 
+const moveToSlide = (carouselInner, currentItem, targetItem) => {
+  carouselInner.style.transform = 'translateX(-' + targetItem.style.left; +')';
+  currentItem.classList.remove('current-item');
+  targetItem.classList.add('current-item');
+  
+}
+
 //when I click left, move slide to left
+prevBtn.addEventListener('click', e => {
+  const currentItem = carouselInner.querySelector('.current-item');
+  const prevItem = currentItem.previousElementSibling;
+
+  moveToSlide(carouselInner, currentItem, prevItem);
+});
 
 //when I click right, move slide right
 nextBtn.addEventListener('click', e => {
   const currentItem = carouselInner.querySelector('.current-item');
   const nextItem = currentItem.nextElementSibling;
-  const amountToMove = nextItem.style.left;
-
-  // move to the next slide
-  carouselInner.style.transform = 'translateX(-' + amountToMove +')';
-  currentItem.classList.remove('current-item');
-  nextItem.classList.add('current-item');
+  moveToSlide(carouselInner, currentItem, nextItem);
   
-  
-})
+});
 
 //when I click nav indicator, move to that slide
+dotsNav.addEventListener('click', e => {
+  //what indicator was clicked on? 
+  const targetDot = e.target.closest('button');
+  
+  if(!targetDot) return;
+
+  const currentItem = carouselInner.querySelector('.current-item');
+  const currentDot = dotsNav.querySelector('.current-item');
+  const targetIndex = dots.findIndex(dot => dot === targetDot);
+
+  console.log(targetIndex)
+  
+})
